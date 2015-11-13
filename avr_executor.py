5cf2446434ef1a7c2b232acfa06cff3b8c907c23
Rodion Gorkovenko <rodiongork@github.com>
2015-11-13 10:32:58 +0300

    Addition and flags

--- avr_executor.py
+++ avr_executor.py
@@ -4,6 +4,13 @@ class Executor(object):
     
     ip = 0
     sp = 0
+    flag_t = 0
+    flag_h = 0
+    flag_s = 0
+    flag_v = 0
+    flag_n = 0
+    flag_z = 0
+    flag_c = 0
     
     def __init__(self, code):
         self.peripherals = Peripherals(self)
@@ -13,6 +20,10 @@ class Executor(object):
     def step(self):
         w = self.words[self.ip]
         w3 = w >> 12
+        if w3 == 0b0000:
+            self.i_0000(w)
+        if w3 == 0b0001:
+            self.i_0001(w)
         if w3 == 0b0010:
             self.i_0010(w)
         if w3 == 0b1011:
@@ -34,10 +45,28 @@ class Executor(object):
         print
         print "ip=%04X, sp=%04X" % (self.ip, self.sp)
     
+    def i_0000(self, w):
+        w2 = (w >> 8) & 0x0F
+        if (w2 >> 2) == 3:
+            self.i_add(w)
+        
+    def i_0001(self, w):
+        w2 = (w >> 8) & 0x0F
+        if (w2 >> 2) == 3:
+            self.i_add(w, True)
+        
     def i_0010(self, w):
         w2 = (w >> 8) & 0x0F
         if (w2 >> 2) == 3:
             self.i_mov(w)
+    
+    def i_add(self, w, with_carry = False):
+        d, r = self.dest5_src5(w)
+        a = self.regs[d]
+        b = self.regs[r]
+        res = (a + b + (self.flag_c if with_carry else 0)) & 0xFF
+        self.set_flags(a, b, res)
+        self.regs[d] = res
     
     def i_in_out(self, w):
         d = (w >> 11) & 0x01
@@ -54,9 +83,28 @@ class Executor(object):
         self.regs[r] = v
     
     def i_mov(self, w):
+        d, r = self.dest5_src5(w)
+        self.regs[d] = self.regs[r]
+    
+    def dest5_src5(self, w):
         d = (w >> 4) & 0x1F
         r = ((w >> 5) & 0x10) | (w & 0xF)
-        self.regs[d] = self.regs[r]
+        return (d, r)
+    
+    def set_flags(self, a, b, res):
+        a3 = (a >> 3) & 1
+        b3 = (b >> 3) & 1
+        nr3 = (~res >> 3) & 1
+        self.flag_h = (a3 & b3) | (nr3 & a3) | (nr3 & b3)
+        a7 = (a >> 7) & 1
+        b7 = (b >> 7) & 1
+        r7 = (res >> 7) & 1
+        nr7 = r7 ^ 1
+        self.flag_v = 1 if a7 == b7 and a7 != r7 else 0
+        self.flag_n = r7
+        self.flag_s = self.flag_v ^ self.flag_n
+        self.flag_z = res == 0
+        self.flag_c = (a7 & b7) | (nr7 & a7) | (nr7 & b7)
 
 class Peripherals(object):
     
@@ -80,6 +128,8 @@ class Peripherals(object):
             return self.io_spl()
         elif port == 0x3E:
             return self.io_sph()
+        elif port == 0x3F:
+            return self.io_sreg()
         else:
             raise ValueError('Unsupported IO register for IN: ' + str(port))
     
@@ -103,4 +153,15 @@ class Peripherals(object):
             ex.sp = (ex.sp & 0xFF) | (value << 8)
         else:
             return (ex.sp >> 8) & 0xFF
-
+    
+    def io_sreg(self):
+        ex = self.executor
+        res = ex.flag_c
+        res |= ex.flag_z << 1
+        res |= ex.flag_n << 2
+        res |= ex.flag_v << 3
+        res |= ex.flag_s << 4
+        res |= ex.flag_h << 5
+        res |= ex.flag_t << 6
+        return res
+    
--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from intel_hex_loader import HexLoader
 from avr_executor import Executor
 
 import sys
-s = sys.stdin.read()
+
+if len(sys.argv) < 2:
+    raise Exception('Usage: python main.py <file.hex> [reg0 reg1 ...]')
+
+regs = [int(x) for x in sys.argv[2:]]
+
+with open(sys.argv[1]) as hexfile:
+    s = hexfile.read()
 hl = HexLoader(8192)
 hl.load(s)
 ex = Executor(hl.words)
+for i in range(len(regs)):
+    ex.regs[i] = regs[i]
 ex.run()
 ex.printRegs()
 
